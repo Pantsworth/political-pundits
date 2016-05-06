8023ea56ca3541f9858baaadb125fc0af7166e40
Joshua Shi <thehandsomepanther@gmail.com>
2016-05-05 22:16:20 -0500

    cleaning

--- context/pundits/pundits.py
+++ context/pundits/pundits.py
@@ -3,9 +3,9 @@ import requests
 import bs4
 from newspaper import Article
 
-def get_relevant_articles(query):
+def retrieve_snippets(query):
     '''
-    takes in a query (keyword) and returns relevant snippets if that keyword exists, else returns false
+    takes in a query (keyword) and returns a list of relevant snippets if that keyword exists, else returns false
     snippets are dictionaries with the following keys:
         name: name of pundit
         keyword: query keyword
@@ -13,6 +13,8 @@ def get_relevant_articles(query):
         url: url to the full text (article or tweet)
         text: body of the snippet
     '''
+
+    snippets = []
 
     with open('panel/panel.json', "r") as json_file:
         panel = json.loads(json_file.read())
@@ -24,21 +26,24 @@ def get_relevant_articles(query):
                         snippet['name'] = user['name']
                         snippet['keyword'] = query
                         url = user['links'][link]
-                        get_relevant_text(query, link, url, snippet)
+                        snippets = build_snippets(query, link, url, snippet)
         else:
             return False
 
+    print snippets
+    return snippets
 
-def get_relevant_text(query, website, url, snippet):
+
+def build_snippets(query, website, url, snippet):
     '''
     takes in the query, website, url, and snippet, and constructs the full snippet
     '''
+    snippets = []
     query = query.lower()
     snippet['source'] = website
     if website == "cfr":
         response = requests.get(url + "#publications")
         soup = bs4.BeautifulSoup(response.text, "html.parser")
-        # print "\t\t" + url + "#publications"
         for article in soup.select('article.publication_spotlight h3 a'):
             href = str(article.attrs.get('href'))
             full_url = "http://www.cfr.org/" + href
@@ -46,11 +51,10 @@ def get_relevant_text(query, website, url, snippet):
             article.download()
             article.parse()
             article.nlp()
-            # print article.keywords
             if query in article.keywords:
                 snippet['text'] = article.summary
                 snippet['url'] = full_url
-                print snippet
+                snippets.append(snippet)
     elif website == "twitter":
         response = requests.get(url)
         soup = bs4.BeautifulSoup(response.text, "html.parser")
@@ -63,4 +67,6 @@ def get_relevant_text(query, website, url, snippet):
                     if query in tweet_content:
                         snippet['text'] = tweet_content
                         snippet['url'] = "http://twitter.com" + tweet_url
-                        print snippet
+                        snippets.append(snippet)
+
+    return snippets
